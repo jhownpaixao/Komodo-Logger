c3a885e1edd7ffb72249b5de85fbdf3e274e7729
Jhonnata Nascimento da Paixão <114832000+jhownpaixao@users.noreply.github.com>
2023-07-11 00:18:16 -0300

    initial

---
+++ src/Logger.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Komodo\Logger;
+
+/*******************************************************************************************
+ EP Exodus Project
+ ____________________________________________________________________________________________
+ *
+ * Desenvolvido por: Jhonnata Paixão (Líder de Projeto)
+ * Iniciado em: 15/10/2022
+ * Arquivo: Logger.php
+ * Data da Criação Sun May 14 2023
+ * Copyright (c) 2023
+ *
+ *********************************************************************************************/
+
+
+class Logger
+{
+    private $name;
+    private $especial;
+    public static function BreackAndLog($data)
+    {
+        echo "<pre>";
+        var_dump($data);
+        echo "</pre>";
+        die();
+    }
+
+    public static function BreackAndLogAPI($log, $msg = 'Houve uma falha na requisição')
+    {
+        echo json_encode([
+            'error' => true,
+            'error_msg' => $msg
+
+        ]);
+        die();
+    }
+
+    public static function error($data, $msg = null)
+    {
+    }
+
+    public static function info($data, $msg = null)
+    {
+    }
+
+    public static function warn($data, $msg = null)
+    {
+    }
+
+    public static function trace($data, $msg = null)
+    {
+    }
+
+    public static function all($data, $msg = null)
+    {
+    }
+
+    public static function debug($data, $msg = null)
+    {
+    }
+
+    public function register($name)
+    {
+        $this->name = $name;
+    }
+}
